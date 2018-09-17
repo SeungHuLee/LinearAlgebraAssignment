54937be64d23dbb9dc9625f3bbbda099f5e9b602
SeungHuLee <gadlsh12081208@gmail.com>
2018-09-17 12:33:40 +0900

    make function that fetches input

--- 소스.cpp
+++ 소스.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <array>
 using namespace std;
+array<array<int, 7>, 7> row;
+
+void input() {
+	array<int, 7> temp;
+	cout << "Input your coefficient matrix : " << endl;
+	cin >> temp[0] >> temp[1] >> temp[2] >> temp[3] >> temp[4] >> temp[5] >> temp[6];
+
+	// 일단 첫째 row부터 입력받은 값으로 채워넣어 보자. 7x7 행렬이다.
+	for (int i = 0; i < 1; i++) {
+		row[i] = temp;
+	}
+
+}
+
 int main() {
-	//cout << "Input your coefficient matrix : " << endl;
-	array<array<int, 7>, 7> row;
+	input();
 	int count = 0;
 
 	for (auto& column : row) {
 		
 		for (auto& element : column) {
-			element = count++;
+			
 			
 			cout << element << " ";
 		}
 		cout << endl;
 	}
-	cout << row[0][0] << row[0][1] << endl;
+	//cout << row[0][0] << row[0][1] << endl;
 }
